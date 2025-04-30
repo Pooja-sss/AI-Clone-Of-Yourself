98a4f1b0c7d39e886ca403e473b8127171f3ae71
Pooja-sss <143308668+Pooja-sss@users.noreply.github.com>
2025-04-30 14:34:00 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
